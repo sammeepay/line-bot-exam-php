19aca3f24f74f8246d440db2ac83d7b4422a0ccb
SamMeePay <sammeepay@hotmail.com>
2021-02-12 10:06:36 +0700

    update webhooks

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'atTwNJphyGPgdj3qVBN/Kq8FSSnvRJj+HdbTRMX7QZFKc1yly2h7tz+Q1aPJ6s3owmOZH9Gx0L4k0q71D2ubwL+EZeI5QJvMJCjy+a/2NdOGl9z40xbsBsDQRcfcetbzQU/ZItcO/iJhOKkQdNdbOwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
