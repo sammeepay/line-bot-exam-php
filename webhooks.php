5d8a98d36aacba64285b84ff5006177b7d5d5162
SamMeePay <sammeepay@hotmail.com>
2021-02-12 19:55:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -132,31 +132,31 @@ $msg_to_line .= "ตำแหน่ง longitude : ".$rs->data[0]->box_longitude
 			$result = curl_exec($ch);
 			curl_close($ch);
 			
-			$messages = [
-				'type' => 'location',
-				'title' => ''.$rs->data[0]->position_name.'',
-				'address' => 'nulled',
-				'latitude' => ''.$rs->data[0]->box_latitude.'',
-				'longitude' => ''.$rs->data[0]->box_longitude.''
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+			//$messages = [
+			//	'type' => 'location',
+			//	'title' => ''.$rs->data[0]->position_name.'',
+			//	'address' => 'nulled',
+			//	'latitude' => ''.$rs->data[0]->box_latitude.'',
+			//	'longitude' => ''.$rs->data[0]->box_longitude.''
+			//];
+			//
+			//// Make a POST Request to Messaging API to reply to sender
+			//$url = 'https://api.line.me/v2/bot/message/reply';
+			//$data = [
+			//	'replyToken' => $replyToken,
+			//	'messages' => [$messages],
+			//];
+			//$post = json_encode($data);
+			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			//
+			//$ch = curl_init($url);
+			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			//$result = curl_exec($ch);
+			//curl_close($ch);
 
 			echo $result . "\r\n";
 		}
